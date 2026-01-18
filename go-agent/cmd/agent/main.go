cd8bb8963b5aa8e7cb13bf91b123e471e172472d
tonhowtf <tonhowtf@gmail.com>
2026-01-18 13:04:51 -0300

    feat: parser lockfile

--- go-agent/cmd/agent/main.go
+++ go-agent/cmd/agent/main.go
@@ -1,63 +1,43 @@
 package main
 
 import (
-	"unsafe"
-
-	"golang.org/x/sys/windows"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
-type Process struct {
-		PID  uint32
-		Name string
-	}
-
-func main() {
-	
+func main(){
+	GetLockFile()
 }
 
 
-func FindProcessByName(name string) (*Process, error) {
-	processes, err := ListProcesses()
-	if err != nil {
-		return nil, err
+func GetLockFile() {
+	cmd := exec.Command("powershell", "-NoProfile", "-Command", "(Get-Process LeagueClientUx).Path")
+	output, err := cmd.Output()
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(string(output))
+
+	path := GetDir(string(output)) 
+
+	lockfile, err := ReadLockFile(path)
+
+	if err != nil{
+		panic(err)
 	}
 
-	for _, p := range processes {
-		if p.Name == name {
-			return &p, nil
-		}
-	}
-
-	return nil, nil
+	println(string(lockfile))
 }
 
-func ListProcesses() ([]Process, error) {
-	snap, err := windows.CreateToolhelp32Snapshot(windows.TH32CS_SNAPPROCESS, 0)
-	if err != nil {
-		return nil, err
-	}
 
-	defer windows.CloseHandle(snap)
-
-	var entry windows.ProcessEntry32
-	entry.Size = uint32(unsafe.Sizeof(entry))
-
-	if err = windows.Process32First(snap, &entry); err != nil {
-		return nil, err
-	}
-
-	var processes []Process
-
-	for {
-		processes = append(processes, Process{
-			PID:  entry.ProcessID,
-			Name: windows.UTF16ToString(entry.ExeFile[:]),
-		})
-
-		if err = windows.Process32Next(snap, &entry); err != nil {
-			break
-		}
-	}
-
-	return processes, nil
+func GetDir(leagueUX string) string{
+	return filepath.Dir(leagueUX)
 }
+
+func ReadLockFile(path string) ([]byte, error){
+	filePath := filepath.Join(path, "lockfile")
+	return os.ReadFile(filePath)
+}
+
--- go-agent/internal/lcu/lockfile.go
+++ go-agent/internal/lcu/lockfile.go
@@ -1,7 +1,1 @@
 package lcu
-
-func FindProcess() (uint32, error) {
-	return 0, nil
-}
-
-
