8fcfd33f504089df9a4fd0368daab2d88d0dbc56
tonhowtf <tonhowtf@gmail.com>
2026-01-18 13:10:23 -0300

    refactor: modularize

--- go-agent/cmd/agent/main.go
+++ go-agent/cmd/agent/main.go
@@ -1,43 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
-)
-
-func main(){
-	GetLockFile()
+import "github.com/tonhowtf/lol-agent/internal/lcu"
+func main() {
+	lcu.GetLockFile()
 }
-
-
-func GetLockFile() {
-	cmd := exec.Command("powershell", "-NoProfile", "-Command", "(Get-Process LeagueClientUx).Path")
-	output, err := cmd.Output()
-	if err != nil{
-		panic(err)
-	}
-	fmt.Println(string(output))
-
-	path := GetDir(string(output)) 
-
-	lockfile, err := ReadLockFile(path)
-
-	if err != nil{
-		panic(err)
-	}
-
-	println(string(lockfile))
-}
-
-
-func GetDir(leagueUX string) string{
-	return filepath.Dir(leagueUX)
-}
-
-func ReadLockFile(path string) ([]byte, error){
-	filePath := filepath.Join(path, "lockfile")
-	return os.ReadFile(filePath)
-}
-
--- go-agent/internal/lcu/lockfile.go
+++ go-agent/internal/lcu/lockfile.go
@@ -1,1 +1,36 @@
 package lcu
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func GetLockFile() {
+	cmd := exec.Command("powershell", "-NoProfile", "-Command", "(Get-Process LeagueClientUx).Path")
+	output, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(output))
+
+	path := GetDir(string(output))
+
+	lockfile, err := ReadLockFile(path)
+
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(lockfile))
+}
+
+func GetDir(leagueUX string) string {
+	return filepath.Dir(leagueUX)
+}
+
+func ReadLockFile(path string) ([]byte, error) {
+	filePath := filepath.Join(path, "lockfile")
+	return os.ReadFile(filePath)
+}
